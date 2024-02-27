949a9c561a200215b417cf5dc561706cc3c89c7b
Deni Kolpakov <kolpakovdeni@yandex.ru>
2024-02-27 16:11:52 +0300

    fix: cors, limiter, movieModel

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,8 +1,18 @@
 const rateLimit = require('express-rate-limit');
 
+/* const skipCondition = (req) => {
+  return req.ip === '127.0.0.1';
+}; */
+
+const customHandler = (req, res) => {
+  res.status(429).json({ error: 'Превышен лимит запросов' });
+};
+
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 300,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
+  /* skip: skipCondition, */
+  handler: customHandler,
 });
